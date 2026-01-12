e488cbeb9ae716005df77a5f22e10e37c58dd813
STEPS0N <stepan.klimoff@mail.ru>
2026-01-12 18:54:27 +0500

    Создание класса DbConnection и подключение. Климов

---
+++ Cinema_Klimov/Classes/DbConnection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Cinema_Klimov.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema_Klimov.Classes
+{
+    public class DbConnection : DbContext
+    {
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Poster> Posters { get; set; }
+
+        public DbConnection()
+        {
+            try
+            {
+                Database.EnsureCreated();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;port=3307;database=club_cinema;uid=root;pwd=;", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
--- Cinema_Klimov/MainWindow.xaml.cs
+++ Cinema_Klimov/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Cinema_Klimov.Classes;
 
 namespace Cinema_Klimov
 {
@@ -16,9 +8,13 @@ namespace Cinema_Klimov
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public DbConnection connection = new DbConnection();
+        public MainWindow Init;
+        public MainWindow(MainWindow init)
         {
             InitializeComponent();
+            this.Init = init;
+
         }
     }
 }
