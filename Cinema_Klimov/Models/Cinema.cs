d4a8ab15ec9f9ab79aac7055d308f7ada5928303
Alyonauzhakova <alyonauzhakova@gmail.com>
2026-01-12 18:25:08 +0500

    Добавьте файлы проекта.

---
+++ Cinema_Klimov/Models/Cinema.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Klimov.Models
+{
+    public class Cinema
+    {
+        //[Key]
+        //public int Id { get; set; }
+        //public string Title { get; set; }
+        //public int Halls { get; set; }
+        //public int Places { get; set; }
+    }
+}
---
+++ Cinema_Klimov/Models/Poster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Klimov.Models
+{
+    public class Poster
+    {
+        //[Key]
+        //public int Id { get; set; }
+        //public string Film { get; set; }
+        //public DateTime Time { get; set; }
+        //public int Price { get; set; }
+        //public int IdCinema { get; set; }
+    }
+}
